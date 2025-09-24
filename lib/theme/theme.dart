ca8b8f66e728cad6204f294c4dd87de707af7cbd
pouriashiralipour <pouriashiralipour@gmail.com>
2025-09-24 13:44:25 +0330

    add theme for light and dark

---
+++ lib/theme/theme.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import './colors.dart';
+
+class AppTheme {
+  // Light Theme
+  static ThemeData lightTheme = ThemeData(
+    brightness: Brightness.light,
+    primaryColor: AppColors.primary,
+    scaffoldBackgroundColor: AppColors.white,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: AppColors.primary,
+      foregroundColor: AppColors.white,
+      elevation: 0,
+    ),
+    colorScheme: const ColorScheme.light(
+      primary: AppColors.primary,
+      secondary: AppColors.secondary,
+      error: AppColors.error,
+    ),
+  );
+
+  // Dark Theme
+  static ThemeData darkTheme = ThemeData(
+    brightness: Brightness.dark,
+    primaryColor: AppColors.primary,
+    scaffoldBackgroundColor: AppColors.dark1,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: AppColors.dark2,
+      foregroundColor: AppColors.white,
+      elevation: 0,
+    ),
+    colorScheme: const ColorScheme.dark(
+      primary: AppColors.primary,
+      secondary: AppColors.secondary,
+      error: AppColors.error,
+    ),
+  );
+}
