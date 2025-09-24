8c1d731a18331318cdc9881df7f61f49d40d1cf8
pouriashiralipour <pouriashiralipour@gmail.com>
2025-09-24 22:52:44 +0330

    add timer for next page in welcome

--- lib/screens/welcome/welcome_screen.dart
+++ lib/screens/welcome/welcome_screen.dart
@@ -1,10 +1,31 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 import '../welcome/components/body.dart';
 
-class WelcomeScreen extends StatelessWidget {
+class WelcomeScreen extends StatefulWidget {
   static String routName = './welcome';
   const WelcomeScreen({super.key});
+
+  @override
+  State<WelcomeScreen> createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  Timer? _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    _timer = Timer(const Duration(seconds: 3), () {});
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _timer?.cancel();
+  }
 
   @override
   Widget build(BuildContext context) {
