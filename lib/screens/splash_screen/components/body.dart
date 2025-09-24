3df8e0b4637ed0a69656422d494222858d50dbb0
pouriashiralipour <pouriashiralipour@gmail.com>
2025-09-24 14:54:33 +0330

    add logo to splash screen

--- lib/screens/splash_screen/components/body.dart
+++ lib/screens/splash_screen/components/body.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:full_plants_ecommerce_app/utils/size.dart';
 
 class Body extends StatelessWidget {
   const Body({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Center(child: SvgPicture.asset('assets/images/Logo.svg')),
+          Padding(
+            padding: EdgeInsetsGeometry.only(bottom: SizeConfig.screenHeight * 0.15),
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: SvgPicture.asset('assets/images/progress_bar.svg'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
