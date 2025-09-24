2252c37612e0626e652dc5c7cd6b3a7ae31a8fe0
pouriashiralipour <pouriashiralipour@gmail.com>
2025-09-24 14:27:41 +0330

    add size config

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'screens/splash_screen/splash_screen.dart';
 import 'theme/theme.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
+import 'utils/size.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -13,20 +15,25 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Poteo Application',
-      debugShowCheckedModeBanner: false,
-      localizationsDelegates: [
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: [Locale('fa')],
-      theme: AppTheme.lightTheme,
-      darkTheme: AppTheme.darkTheme,
-      themeMode: ThemeMode.system,
-      routes: routes,
-      initialRoute: SplashScreen.routName,
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        SizeConfig.init(context);
+        return MaterialApp(
+          title: 'Poteo Application',
+          debugShowCheckedModeBanner: false,
+          localizationsDelegates: [
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+          ],
+          supportedLocales: [Locale('fa')],
+          theme: AppTheme.lightTheme,
+          darkTheme: AppTheme.darkTheme,
+          themeMode: ThemeMode.system,
+          routes: routes,
+          initialRoute: SplashScreen.routName,
+        );
+      },
     );
   }
 }
