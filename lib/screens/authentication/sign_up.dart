9662166c578b593ea442fb53c77e8639ef611c28
pouriashiralipour <pouriashiralipour@gmail.com>
2025-09-28 00:03:04 +0330

    add blank auth page

---
+++ lib/screens/authentication/sign_up.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class SignUp extends StatelessWidget {
+  static String routeName = './signUp';
+  const SignUp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
