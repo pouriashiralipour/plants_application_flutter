2a28e7d2d5829b4482252324c5857d7ad5886677
pouriashiralipour <pouriashiralipour@gmail.com>
2025-09-28 02:06:41 +0330

    create custom nav bar with container

--- lib/screens/root/root_screen.dart
+++ lib/screens/root/root_screen.dart
@@ -1,3 +1,4 @@
+import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
@@ -6,8 +7,9 @@ import '../../theme/colors.dart';
 import '../../utils/size.dart';
 
 class RootScreen extends StatefulWidget {
-  static String routeName = './routes';
   const RootScreen({super.key});
+
+  static String routeName = './routes';
 
   @override
   State<RootScreen> createState() => _RootScreenState();
@@ -15,56 +17,52 @@ class RootScreen extends StatefulWidget {
 
 class _RootScreenState extends State<RootScreen> {
   int bottonIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: bottonIndex,
-        onTap: (index) {
-          setState(() {
-            bottonIndex = index;
-          });
-        },
-        showUnselectedLabels: true,
-        type: BottomNavigationBarType.shifting,
-        selectedItemColor: AppColors.primary,
-        unselectedItemColor: AppColors.grey500,
-        selectedLabelStyle: TextStyle(
-          fontSize: SizeConfig.getProportionateScreenWidth(12),
-          fontWeight: FontWeight.w800,
+      bottomNavigationBar: Container(
+        margin: EdgeInsets.only(bottom: SizeConfig.getProportionateScreenHeight(25)),
+        height: SizeConfig.getProportionateScreenHeight(48),
+        width: SizeConfig.screenWidth,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: List.generate(CustomBottomNavigationBarItems.screens().length, (index) {
+            final item = CustomBottomNavigationBarItems.items[index];
+            return Column(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      bottonIndex = index;
+                    });
+                  },
+                  child: SvgPicture.asset(
+                    bottonIndex == index ? item['fill_icon'] : item['icon'],
+                    color: bottonIndex == index ? AppColors.primary : AppColors.grey500,
+                    height: bottonIndex == index
+                        ? SizeConfig.getProportionateScreenWidth(26)
+                        : SizeConfig.getProportionateScreenWidth(24),
+                    width: bottonIndex == index
+                        ? SizeConfig.getProportionateScreenWidth(26)
+                        : SizeConfig.getProportionateScreenWidth(24),
+                  ),
+                ),
+                SizedBox(height: SizeConfig.getProportionateScreenHeight(5)),
+                Text(
+                  item['lable'],
+                  style: TextStyle(
+                    color: bottonIndex == index ? AppColors.primary : AppColors.grey500,
+                    fontSize: bottonIndex == index
+                        ? SizeConfig.getProportionateScreenWidth(12)
+                        : SizeConfig.getProportionateScreenWidth(10),
+                    fontWeight: bottonIndex == index ? FontWeight.w800 : FontWeight.w600,
+                  ),
+                ),
+              ],
+            );
+          }),
         ),
-        unselectedLabelStyle: TextStyle(
-          fontSize: SizeConfig.getProportionateScreenWidth(10),
-          fontWeight: FontWeight.w600,
-        ),
-        items: List.generate(CustomBottomNavigationBarItems.items.length, (index) {
-          final item = CustomBottomNavigationBarItems.items[index];
-          final isSelected = index == bottonIndex;
-          return BottomNavigationBarItem(
-            icon: SvgPicture.asset(
-              item['icon'],
-              width: isSelected
-                  ? SizeConfig.getProportionateScreenWidth(26)
-                  : SizeConfig.getProportionateScreenWidth(24),
-              height: isSelected
-                  ? SizeConfig.getProportionateScreenWidth(26)
-                  : SizeConfig.getProportionateScreenWidth(24),
-              // ignore: deprecated_member_use
-              color: isSelected ? AppColors.primary : AppColors.grey500,
-            ),
-            activeIcon: SvgPicture.asset(
-              item['fill_icon'],
-              width: isSelected
-                  ? SizeConfig.getProportionateScreenWidth(26)
-                  : SizeConfig.getProportionateScreenWidth(24),
-              height: isSelected
-                  ? SizeConfig.getProportionateScreenWidth(26)
-                  : SizeConfig.getProportionateScreenWidth(24),
-              color: isSelected ? AppColors.primary : AppColors.grey500,
-            ),
-            label: item['lable'],
-          );
-        }),
       ),
       body: IndexedStack(index: bottonIndex, children: CustomBottomNavigationBarItems.screens()),
     );
