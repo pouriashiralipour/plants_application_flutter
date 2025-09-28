c2eaee2f2e7870bd29d82320d1bbbf8c88280499
pouriashiralipour <pouriashiralipour@gmail.com>
2025-09-28 13:05:28 +0330

    make new file for custom search bar

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
 import '../../components/custom_app_bar.dart';
+import '../../components/custom_search_bar.dart';
 import '../../theme/colors.dart';
 import '../../utils/size.dart';
 
@@ -45,76 +46,7 @@ class _HomeScreenState extends State<HomeScreen> {
             SizedBox(height: SizeConfig.getProportionateScreenHeight(24)),
             CustomAppBar(isLightMode: isLightMode),
             SizedBox(height: SizeConfig.getProportionateScreenHeight(24)),
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: SizeConfig.getProportionateScreenWidth(24)),
-              child: TextFormField(
-                focusNode: _focusNode,
-                style: TextStyle(
-                  color: isLightMode ? AppColors.grey900 : AppColors.white,
-                  fontWeight: FontWeight.w600,
-                  fontSize: SizeConfig.getProportionateScreenWidth(14),
-                ),
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.symmetric(
-                    horizontal: SizeConfig.getProportionateScreenWidth(16),
-                    vertical: SizeConfig.getProportionateScreenHeight(16),
-                  ),
-                  hintText: "جستجو",
-                  hintStyle: TextStyle(
-                    color: isLightMode ? AppColors.grey400 : AppColors.grey600,
-                    fontSize: SizeConfig.getProportionateScreenWidth(14),
-                    fontFamily: 'IranYekan',
-                  ),
-                  prefixIcon: SizedBox(
-                    width: SizeConfig.getProportionateScreenWidth(60),
-                    child: Center(
-                      child: SvgPicture.asset(
-                        'assets/images/icons/Search.svg',
-                        width: SizeConfig.getProportionateScreenWidth(20),
-                        height: SizeConfig.getProportionateScreenWidth(20),
-                        color: isLightMode
-                            ? _isFocused
-                                  ? AppColors.primary
-                                  : AppColors.grey400
-                            : _isFocused
-                            ? AppColors.white
-                            : AppColors.grey600,
-                      ),
-                    ),
-                  ),
-                  suffixIcon: SizedBox(
-                    width: SizeConfig.getProportionateScreenWidth(60),
-                    child: Center(
-                      child: SvgPicture.asset(
-                        _isFocused
-                            ? 'assets/images/icons/Filter_fill.svg'
-                            : 'assets/images/icons/Filter.svg',
-                        width: SizeConfig.getProportionateScreenWidth(20),
-                        height: SizeConfig.getProportionateScreenWidth(20),
-                        color: AppColors.primary,
-                      ),
-                    ),
-                  ),
-                  filled: true,
-                  fillColor: isLightMode
-                      ? _isFocused
-                            ? AppColors.primary.withValues(alpha: 0.08)
-                            : AppColors.grey100
-                      : _isFocused
-                      ? AppColors.primary.withValues(alpha: 0.08)
-                      : AppColors.dark2,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(16),
-                    borderSide: BorderSide.none,
-                  ),
-
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(16),
-                    borderSide: BorderSide(color: AppColors.primary, width: 1.5),
-                  ),
-                ),
-              ),
-            ),
+            CustmoSearchBar(focusNode: _focusNode, isLightMode: isLightMode, isFocused: _isFocused),
           ],
         ),
       ),
