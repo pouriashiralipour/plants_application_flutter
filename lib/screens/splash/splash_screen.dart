474c202f4d962ee5cd14ca580c5f75bd47e1d5dc
pouriashiralipour <pouriashiralipour@gmail.com>
2025-09-28 12:52:47 +0330

    make svg progress bar animated

--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -1,11 +1,11 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:full_plants_ecommerce_app/utils/size.dart';
 import '../../services/intro_prefs.dart';
 
 import '../root/root_screen.dart';
 import '../welcome/welcome_screen.dart';
-import 'components/body.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -16,12 +16,21 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
-  @override
+class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
   @override
   void initState() {
     super.initState();
     _decideNext();
+
+    _controller = AnimationController(duration: const Duration(seconds: 2), vsync: this)..repeat();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   Future<void> _decideNext() async {
@@ -39,6 +48,23 @@ class _SplashScreenState extends State<SplashScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return const Body();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Spacer(),
+            Center(child: SvgPicture.asset('assets/images/Logo.svg')),
+            Spacer(),
+            Padding(
+              padding: EdgeInsets.only(bottom: SizeConfig.getProportionateScreenHeight(60)),
+              child: RotationTransition(
+                turns: _controller,
+                child: SvgPicture.asset('assets/images/progress_bar.svg'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
