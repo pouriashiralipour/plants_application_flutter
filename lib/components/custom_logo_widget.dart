7ec0980c2b1620af0c44013a4abcd8b5ba5b2338
pouriashiralipour <pouriashiralipour@gmail.com>
2025-09-29 14:10:49 +0330

    create custom logo widget

---
+++ lib/components/custom_logo_widget.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../theme/colors.dart';
+import '../utils/size.dart';
+
+class CustomLogoWidget extends StatelessWidget {
+  const CustomLogoWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final bool isLightMode = Theme.of(context).brightness == Brightness.light;
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          'hiloropia',
+          style: TextStyle(
+            color: isLightMode ? AppColors.grey900 : AppColors.white,
+            fontWeight: FontWeight.w900,
+            fontSize: SizeConfig.getProportionateScreenWidth(48),
+          ),
+        ),
+        SizedBox(width: SizeConfig.getProportionateScreenWidth(10)),
+        SvgPicture.asset(
+          'assets/images/Logo.svg',
+          height: SizeConfig.getProportionateScreenWidth(60),
+          width: SizeConfig.getProportionateScreenWidth(60),
+        ),
+      ],
+    );
+  }
+}
