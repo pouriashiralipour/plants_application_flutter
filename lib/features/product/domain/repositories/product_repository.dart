15281efe389465afcc04c1c7b74d27bc329dfbe6
pouriashiralipour <pouriashiralipour@gmail.com>
2025-12-24 02:17:57 +0330

    feat(product-domain): add ProductRepository contract

---
+++ lib/features/product/domain/repositories/product_repository.dart
@@ -0,0 +1,10 @@
+import '../entities/category.dart';
+import '../entities/product.dart';
+
+abstract class ProductRepository {
+  Future<List<Category>> getCategories();
+
+  Future<Product> getProductById(String id);
+
+  Future<List<Product>> getProducts({String? categoryId, String? search, int? page});
+}
